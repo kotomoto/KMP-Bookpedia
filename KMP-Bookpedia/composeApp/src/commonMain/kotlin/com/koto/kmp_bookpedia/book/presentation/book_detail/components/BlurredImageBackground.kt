7a96c41b1dce00d23183fa28c2df740d85d43719
kotomoto <jkotor@gmail.com>
2025-04-22 10:53:11 +0200

    Add book details to UI

--- KMP-Bookpedia/composeApp/src/commonMain/kotlin/com/koto/kmp_bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
+++ KMP-Bookpedia/composeApp/src/commonMain/kotlin/com/koto/kmp_bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
@@ -1,26 +1,52 @@
 package com.koto.kmp_bookpedia.book.presentation.book_detail.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
 import com.koto.kmp_bookpedia.core.presentation.DarkBlue
 import com.koto.kmp_bookpedia.core.presentation.DesertWhite
+import com.koto.kmp_bookpedia.core.presentation.SandYellow
 import kmp_bookpedia.composeapp.generated.resources.Res
 import kmp_bookpedia.composeapp.generated.resources.book_cover
+import kmp_bookpedia.composeapp.generated.resources.book_error_2
+import kmp_bookpedia.composeapp.generated.resources.go_back
+import kmp_bookpedia.composeapp.generated.resources.mask_as_favorite
+import kmp_bookpedia.composeapp.generated.resources.remove_from_favorites
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -51,7 +77,7 @@ fun BlurredImageBackground(
         }
     )
 
-    Box {
+    Box(modifier = modifier) {
         Column(
             modifier = Modifier.fillMaxSize(),
         ) {
@@ -61,14 +87,14 @@ fun BlurredImageBackground(
                     .fillMaxWidth()
                     .background(DarkBlue)
             ) {
-                    Image(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .blur(20.dp),
-                        painter = painter,
-                        contentDescription = stringResource(Res.string.book_cover),
-                        contentScale = ContentScale.Crop,
-                    )
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .blur(20.dp),
+                    painter = painter,
+                    contentDescription = stringResource(Res.string.book_cover),
+                    contentScale = ContentScale.Crop,
+                )
             }
             Box(
                 modifier = Modifier
@@ -76,6 +102,90 @@ fun BlurredImageBackground(
                     .fillMaxWidth()
                     .background(DesertWhite)
             )
+        }
+
+        IconButton(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(top = 16.dp, start = 16.dp)
+                .statusBarsPadding(),
+            onClick = onBackClick,
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = stringResource(Res.string.go_back),
+                tint = Color.White,
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.fillMaxHeight(0.15f))
+            ElevatedCard(
+                modifier = Modifier
+                    .height(230.dp)
+                    .aspectRatio(2 / 3f),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.elevatedCardColors(
+                    contentColor = Color.Transparent,
+                ),
+                elevation = CardDefaults.elevatedCardElevation(
+                    defaultElevation = 15.dp,
+                ),
+            ) {
+                AnimatedContent(
+                    targetState = imageLoadResult,
+                ) { result ->
+                    when (result) {
+                        null -> CircularProgressIndicator()
+                        else -> {
+                            Box {
+                                Image(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .background(Color.Transparent),
+                                    painter = if (result.isSuccess) painter else {
+                                        painterResource(Res.drawable.book_error_2)
+                                    },
+                                    contentDescription = stringResource(Res.string.book_cover),
+                                    contentScale = if (result.isSuccess) ContentScale.Crop else ContentScale.Fit,
+                                )
+
+                                IconButton(
+                                    modifier = Modifier
+                                        .align(Alignment.BottomEnd)
+                                        .background(
+                                            brush = Brush.radialGradient(
+                                                colors = listOf(
+                                                    SandYellow, Color.Transparent,
+                                                ),
+                                                radius = 70f,
+                                            )
+                                        ),
+                                    onClick = onFavoriteClick,
+                                ) {
+                                    Icon(
+                                        imageVector = if (isFavorite) {
+                                            Icons.Filled.Favorite
+                                        } else {
+                                            Icons.Outlined.FavoriteBorder
+                                        },
+                                        tint = Color.Red,
+                                        contentDescription = if (isFavorite) {
+                                            stringResource(Res.string.remove_from_favorites)
+                                        } else {
+                                            stringResource(Res.string.mask_as_favorite)
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            content()
         }
     }
 }
